3e7ae1b02336ce6d6eefd0f0b702390cf6ad1a2c
Sri Sarat Chaitanya <srisaratchaitanya@gmail.com>
2024-08-31 20:22:38 -0400

    Creating AWS EC2 and Git Repo using Terraform

---
+++ Creating_EC2_and_GitRepo/main.tf
@@ -0,0 +1,35 @@
+#create ec2 instances
+resource "aws_instance" "instance1" {
+  ami           = "ami-066784287e358dad1"
+  instance_type = "t2.micro"
+  provider = aws.east
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+#create ec2 instances
+resource "aws_instance" "instance2" {
+  ami           = "ami-05134c8ef96964280"
+  instance_type = "t2.micro"
+  provider = aws.west
+
+  tags = {
+    Name = "HelloWorld2"
+  }
+}
+
+resource "github_repository" "repo1" {
+  name        = "Test_Repo"
+  description = "This repo was created using Terraform"
+
+  visibility = "public"
+}
+
+resource "github_repository" "repo2" {
+  name        = "Test_Repo_2"
+  description = "This repo was created using Terraform"
+
+  visibility = "private"
+}
